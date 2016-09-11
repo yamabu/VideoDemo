5154b80687452a8927d04acd051a3d5ad30e6e08
kirsting <61427587@qq.com>
2016-09-11 21:22:13 +0800

    try to add touch event support

--- app/src/main/java/moe/yamabu/videodemo/ijk/AndroidMediaController.java
+++ app/src/main/java/moe/yamabu/videodemo/ijk/AndroidMediaController.java
@@ -21,13 +21,20 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.MediaController;
+import android.widget.SeekBar;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class AndroidMediaController extends MediaController implements IMediaController {
     private ActionBar mActionBar;
+    private SeekBar mSeekBar;
+    private float xInAll;
+    private int xMocked;
 
     public AndroidMediaController(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -45,6 +52,7 @@ public class AndroidMediaController extends MediaController implements IMediaCon
     }
 
     private void initView(Context context) {
+
     }
 
     public void setSupportActionBar(@Nullable ActionBar actionBar) {
@@ -82,5 +90,54 @@ public class AndroidMediaController extends MediaController implements IMediaCon
         mShowOnceArray.add(view);
         view.setVisibility(View.VISIBLE);
         show();
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        SeekBar seekBar = getSeekBar();
+        if (seekBar == null) {
+            return super.dispatchTouchEvent(ev);
+        }
+        int action = ev.getAction();
+        //按下
+        if (action == MotionEvent.ACTION_DOWN) {
+            xInAll = ev.getX();
+            xMocked=getMockedX(seekBar);
+            Log.d("TouchTest","xInAll= "+xInAll+" xMocked="+xMocked );
+            MotionEvent newEv = MotionEvent.obtain(ev.getDownTime(), ev.getEventTime(), ev.getAction(), xMocked, ev.getY(), ev.getMetaState());
+            seekBar.onTouchEvent(newEv);
+        }else {
+            int newX= ((int) ((ev.getX() - xInAll) / 3))+xMocked;
+            Log.d("TouchTest","xInAll= "+xInAll+" newX="+newX );
+            MotionEvent newEv = MotionEvent.obtain(ev.getDownTime(), ev.getEventTime(), ev.getAction(), newX, ev.getY(), ev.getMetaState());
+            seekBar.onTouchEvent(newEv);
+        }
+        Log.d("dispatchTouchEvent", ev.toString() + "  media controller ");
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private int getMockedX(SeekBar seekBar) {
+        float per = ((float) seekBar.getProgress()) / seekBar.getMax();
+        int width = seekBar.getWidth();
+        int paddingLeft = seekBar.getPaddingLeft();
+        int paddingRight = seekBar.getPaddingRight();
+        int available = width - paddingLeft - paddingRight;
+        return paddingLeft + ((int) (available * per));
+    }
+
+    public SeekBar getSeekBar() {
+        if (mSeekBar == null) {
+            try {
+                Field mProgressField = MediaController.class.getDeclaredField("mProgress");
+                mProgressField.setAccessible(true);
+                Object o = mProgressField.get(this);
+                if (o instanceof SeekBar) {
+                    mSeekBar = (SeekBar) o;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return mSeekBar;
     }
 }
